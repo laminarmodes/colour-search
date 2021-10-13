1b3867baaf2eedda2b9fb7ae8865e63858630e42
laminarmodes <anya.traille@gmail.com>
2021-10-13 15:34:04 +0800

    Pokemon Array Task

---
+++ README.md
@@ -0,0 +1,9 @@
+# Notes: This will include basics of how someone can engage with the project: what it is, how to set it up, which development environment to use, what dependencies to watch out for, etc.
+
+# Topics covered in a project setup doc can range from technical requirements to the libraries used, necessary tools, and connected database.
+
+# Other things to include are: 
+# Project description
+# How to get the project running
+# Project dependencies (e.g., JavaScript version, ESLint rules)
+# Which API the project uses
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
-alert('Hello world');
+/* Practice */
+/* alert('Hello world');
 let myGreeating = 'Hello world again';
 document.write(myGreeating);
 let favoriteFood = "Gyoza";
 document.write(favoriteFood);
+*/
+
+pokemonList = [];
+
+let testPokemon1 = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    abilities: ['chlorophyll', 'overgrow'],
+    eggGroups: ['monster', 'grass']
+}
+
+let testPokemon2 = {
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9,
+    abilities: ['rain-dish', 'torrent'],
+    eggGroups: ['monster', 'water 1']
+}
+
+let testPokemon3 = {
+    name: 'Persian',
+    height: 1,
+    weight: 32,
+    abilities: ['limber', 'technician', 'unnerve'],
+    eggGroups: ['field']
+}
+
+pokemonList = [testPokemon1, testPokemon2, testPokemon3];
+
+document.write(testPokemon1.name)
+document.write("<br>")
+document.write(pokemonList[0].name)
